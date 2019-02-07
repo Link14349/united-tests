bcccd91d1df3e7ab628b9369c94bafb69c5e535c
yhzheng <32408362+qianduanXIAOHAOZI@users.noreply.github.com>
2019-02-07 10:17:04 +0800

    Release version v-1.0.1
    Pulished in npm and yarn

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # united-tests
-This is a tool that can automate and validate unit tests
+This is a tool that can automate and validate unit tests.
+
+## _Why use them?_
+Because this is a lightweight and powerful unit test gadget, and we will continue to maintain it in the future.
+
+## Download
+### _Download with npm_
+```bash
+sudo npm install united-tests -g
+```
+### _Download with yarn_
+```bash
+sudo yarn add united-tests -g
+```
+
+## Usage
+```bash
+unitedt [options]
+```
+### _Flags_
+- -h, --help: __output usage information__
+- -V, --version: __output the version number__
+- -e, --error: __show detailed error message when got error__
+- -o, --out: __open test-out flag__
+- -p, --put: __open put-json flag(This flag can output some info in a json file)__
+- -i, --info [filename]: __Set the out info json file (default: "tests-info.json")__
+- -j, --json [filename]: __Set the entry json file (default: "tests.json")__
+### Example
+tests.json
+```json
+{
+  "test-out": false,
+  "tests": [
+    {
+      "name": "hello",
+      "run": {
+        "inputs": [
+          []
+        ],
+        "outputs": [
+          "Hello world!"
+        ]
+      }
+    }
+  ]
+}
+```
+```bash
+unitedt
+```
+
+### Edit tests.json
+#### _Basic structure_
+```json
+{
+  "test-out": boolean (default: false),
+  "out-in-json": boolean (default: false),
+  "out-json-name": string (default: "tests.json"),
+  "read-dir": string (default: "tests-info.json"),
+  "tests": array
+}
+```
+__Prototypes__  
+- test-out _Set whether to output the result of the test module_
+- out-in-json _Set whether to output the test results and summary to the json file_
+- out-in-json _Set json filename_
+- read-dir _Set the path to the unit test summary_
+- tests _Set up each module that the unit test needs to test and its input and output samples_
+
+#### Element of tests
+```json
+{
+  "name": string,
+  "run": {
+    "inputs": array,
+    "outputs": array
+  }
+}
+```
+
+## License
+MIT
---
+++ bin/main.js
@@ -0,0 +1,127 @@
+#!/usr/bin/env node
+let program = require('commander');
+const chalk = require('chalk');
+const fs = require("fs");
+const path = require("path");
+
+program
+    .version('1.0.0')
+    .option("-e, --error", "show detailed error message")
+    .option("-o, --out", "open test-out flag")
+    .option("-p, --put", "open put-json flag(This flag can output some info in a json file)")
+    .option("-i, --info [filename]", "Set the out info json file", "tests-info.json")
+    .option("-j, --json [filename]", "Set the entry json file", "tests.json")
+    .description('This is a tool that can automate and validate unit tests.');
+program.parse(process.argv);
+
+function main() {
+    let testEntry = program.json;
+    console.log(chalk.bold("untied-tests command line tool"));
+    console.log(chalk.cyan("[INFO] Reading '" + testEntry + "'..."));
+    let testsJSON;
+    try {
+        testsJSON = fs.readFileSync(path.join(process.cwd(), testEntry)).toString();
+    } catch (e) {
+        console.log(chalk.red.bold("[ERROR] Read '" + testEntry + "' failed."));
+        if (program.error) {
+            console.log("Detailed error message: \n" + chalk.red(JSON.stringify(e, null, 2)));
+        } else {
+            console.log(chalk.red("Add flag -e or --error to show detailed error message."));
+        }
+        console.log("exit.");
+        return;
+    }
+    console.log(chalk.green("[FINISH] Read '" + testEntry + "' success."));
+    console.log(chalk.cyan("[INFO] Parsing '" + testEntry + "'..."));
+    try {
+        testsJSON = JSON.parse(testsJSON);
+    } catch (e) {
+        console.log(chalk.red.bold("[ERROR] Parse tests.json failed."));
+        if (program.error) {
+            console.log("Detailed error message: \n" + chalk.red(JSON.stringify(e, null, 2)));
+        } else {
+            console.log(chalk.red("Add flag -e or --error to show detailed error message."));
+        }
+        console.log("exit.");
+        return;
+    }
+    console.log(chalk.green("[FINISH] Parse '" + testEntry + "' success."));
+    let {
+        "read-dir": readDir = "tests",
+        "test-out": testOut = program.out,
+        "out-in-json": outJSON = program.put,
+        "out-json-name": outJSONName = program.info,
+        tests
+    } = testsJSON;
+    let infoJSON = {
+        "main-score": 0,
+        "tests": []
+    };
+    let mainScore = 0;
+    for (let i of tests) {
+        let name = i["name"];
+        let inputs = i["run"]["inputs"];
+        let outputs = i["run"]["outputs"];
+        let point = 100 / inputs.length;
+        let score = 0;
+        let failed = [];
+        let success = [];
+        let test;
+        // console.log("./" + readDir + "/" + name);
+        console.log(chalk.cyan("[INFO] Running test module '" + name + "' in directory '" + readDir + "'..."));
+        let modulePath = path.join(process.cwd(), readDir, name);
+        try {
+            test = require(modulePath);
+        } catch (e) {
+            console.log(chalk.red.bold("[ERROR] Import test module '" + name + "' in directory '" + readDir + "' failed."));
+            console.log("Module path: " + modulePath);
+            if (program.error) {
+                console.log("Detailed error message: \n" + chalk.red(JSON.stringify(e, null, 2)));
+            } else {
+                console.log(chalk.red("Add flag -e or --error to show detailed error message."));
+            }
+            console.log("exit.");
+            return;
+        }
+        for (let j in inputs) {
+            let out = test(...inputs);
+            if (testOut) console.log(out);
+            if (out == outputs[j]) {
+                score += point;
+                success.push(j);
+            } else {
+                failed.push(j);
+            }
+        }
+        console.log(chalk.yellow("[INFO] Test '" + name + "' got score: " + score + ""));
+        if (score == 100) {
+            console.log(chalk.green("[FINISH] Test '" + name + "' finished all!"));
+        } else {
+            for (let j in success) {
+                console.log(chalk.green("[FINISH] Test '" + name + "' passed test point " + j));
+            }
+            for (let j in failed) {
+                console.log(chalk.yellow("[FINISH] Test '" + name + "' failed test point " + j));
+            }
+        }
+        mainScore += score;
+        if (outJSON) {
+            infoJSON.tests.push({
+                "name": name,
+                "score": score,
+                "one-test-point": point,
+                "module-path": modulePath,
+                "failed": failed,
+                "success": success,
+            });
+        }
+    }
+    if (outJSON) {
+        infoJSON["main-score"] = mainScore;
+        fs.writeFileSync(path.join(process.cwd(), outJSONName), JSON.stringify(infoJSON, null, 2));
+        console.log("Look info in '" + outJSONName + "'.");
+    }
+    console.log(chalk.green.bold.bgBlack("[FINISH]"));
+}
+
+main();
